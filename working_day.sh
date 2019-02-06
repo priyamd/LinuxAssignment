5df21cb037f962ed842e760904c99a3856138f19
Priyanka <priyankadhamal1@gmail.com>
2019-02-06 16:26:51 +0530

    Write Script which when executed checks out whether it is a working day or not

---
+++ working_day.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+DAYOFWEEK=$(date +"%u")
+echo "Day of week :$DAYOFWEEK"
+
+if [ $DAYOFWEEK == 1 ]
+then
+        echo "Working Day.. ;)"
+fi
+
+if [ $DAYOFWEEK == 2 ]
+then
+        echo "Working Day.. ;)"
+fi
+
+if [ $DAYOFWEEK == 3 ]
+then
+        echo "Working Day.. ;)"
+fi
+
+if [ $DAYOFWEEK == 4 ]
+then
+        echo "Working Day.. ;)"
+fi
+
+if [ $DAYOFWEEK == 5 ]
+then
+        echo "Working Day.. ;)"
+fi
+
+if [ $DAYOFWEEK == 6 ]
+then
+        echo "Weekoff.. :)"
+fi
+
+if [ $DAYOFWEEK == 7 ]
+then
+        echo "Weekoff.. :)"
+fi
+
